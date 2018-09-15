1f01a5f68daf501a4dd48d5b80b9c6e0406e9a9b
New Author Name <dan.sajjad@gmail.com>
2018-09-15 21:34:42 +0000

    Create controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'ccsf_rails/index'
+
+  get 'ccsf_rails/links'
+
+  get 'ccsf_rails/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
