e1c8e41d7ed353d723494bfd66a71a5e9fd357c7
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-13 20:42:35 +0000

    Name and email presence validations and test cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    # ADDING VALIDATION FOR NAME
+    # same as writing validates(:name, {presence: true}) 
+    # same as writing validates(:name, presence: true)
+    # .valid? method return if all validations pass
+    # .errors.full_messages displays messages
+    # user will not save to db if invalid
+    # Rails validates the presence of an attribute using the .blank? method
+    validates :name, presence: true
+    validates :email, presence: true
 end
