4abe711b131e3922256c8b44a148b5b61ca69998
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-14 03:30:16 +0000

    Added a secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,6 @@ class User < ApplicationRecord
         format: { with: VALID_EMAIL_REGEX },
         # uniqueness: true
         uniqueness: { case_sensitive: false }
+    
+    has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'Test user', email: 'testuser@gmail.com')
+    @user = User.new(
+      name: 'Test user',
+      email: 'testuser@gmail.com',
+      password: 'foobar',
+      password_confirmation: 'foobar'
+    )
   end
   
   test 'should be valid' do
