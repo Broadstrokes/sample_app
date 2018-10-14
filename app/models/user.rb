69a94e06dc958fcf552ad04971c50c3413c47664
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-14 02:46:09 +0000

    Add format validation for email; add test cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ApplicationRecord
     # .errors.full_messages displays messages
     # user will not save to db if invalid
     # Rails validates the presence of an attribute using the .blank? method
+    
+    # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    
     validates :name, presence: true, length: { maximum: 50 }
-    validates :email, presence: true, length: { maximum: 255 }
+    validates :email, 
+        presence: true,
+        length: { maximum: 255 },
+        format: { with: VALID_EMAIL_REGEX }
 end
