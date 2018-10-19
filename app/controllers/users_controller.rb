7778e51cd504ef2bb94a2ef9eb9631ae4dd87a6f
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-19 07:33:23 +0000

    User strong parameters in the controller when creating a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,25 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save # returns true or false
       # handle successful save
     else
       render 'new'
     end
   end
+  
+  private 
+    
+    def user_params
+      # This code returns a version of the params hash with only
+      # the permitted attributes (while raising an error if the 
+      # :user attribute is missing)
+      params.require(:user).permit(
+        :name,
+        :email,
+        :password,
+        :password_confirmation
+      )
+    end
 end
