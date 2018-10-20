d32b7ef71bb81e7fbb427e10030367e2f6f449e3
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-20 04:28:36 +0000

    Write integration test for successful sign up

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,7 +7,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'form[action="/signup"]'
 
     assert_no_difference 'User.count' do
-      post signup_path, params:  {
+      post signup_path, params: {
       user: {
         name: '',
         email: 'user@invalid',
@@ -20,5 +20,29 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
+  end
+  
+  
+  test 'successful signup' do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    
+    assert_difference 'User.count', 1 do
+      post signup_path, params: {
+        user: {
+          name: 'Pink Panther',
+          email: 'panther@gmail.com',
+          password: 'foobar',
+          password_confirmation: 'foobar'
+        }
+      }
+    end
+    # This simply arranges to follow the redirect after submission,
+    # resulting in a rendering of the 'users/show' template
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+    assert_select 'div.alert-success'
+    
   end
 end
