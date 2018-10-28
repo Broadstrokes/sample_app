0b7bd76a6ac48dc036a647d4036954cd0dfafc35
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-28 06:40:57 +0000

    Add session helper method log_in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,12 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # log the user & redirect to users who page
+      log_in(user)
+      
+      # NB: the compact redirect redirect_to user
+      # Rails automatically converts this to the route for the 
+      # user’s profile page: user_url(user)
+      redirect_to user
     else 
       # create an error message
       flash.now[:danger] = 'Invalid email/password combination'
