615dd4b95ac37b489c3f9958121b7e8d6f32ce73
Dan Sajjad <dan.sajjad@gmail.com>
2018-11-13 06:04:03 +0000

    By issuing a PATCH request directly to the user path, verify that the admin attribute
    isn’t editable through the web.
    
    if we simply passed an initialization hash in from an arbitrary web request,
    a malicious user could send a PATCH request as follows:
    
    patch /users/17?admin=1
    
    This request would make user 17 an admin, which would be a potentially serious security breach.
    Because of this danger, it is essential that we only update attributes that are safe to edit
    through the web. As noted in Section 7.3.2, this is accomplished using strong parameters by
    calling require and permit on the params hash:
    
      private
    
    patch /users/17?admin=1
    
    Because of this danger, it is essential that we only update attributes that are safe to edit through the web.
    As noted in Section 7.3.2, this is accomplished using strong parameters by calling require and permit
    on the params hash:
    
    private
    def user_params
      params.require(:user).permit(:name, :email, :password, :password_confirmation)
    end

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -32,4 +32,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_path
     assert_redirected_to login_url
   end
+  
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: { 
+        password: '',
+        password_confirmation: '',
+        admin: true
+      } 
+    }
+    # You must reload the instance variable after making 
+    # changes to the underlying record. This will load in 
+    # the new changes
+    assert_not @other_user.reload.admin?
+  end
 end
