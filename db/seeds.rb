68a0e81cab9d576934ac4753d07844488f4e0e2b
Dan Sajjad <dan.sajjad@gmail.com>
2018-11-13 05:43:58 +0000

    Seed data code with the 1st user as admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,8 @@ User.create!(
     name: "Example User",
     email: "example@railstutorial.org",
     password: "foobar",
-    password_confirmation: "foobar"
+    password_confirmation: "foobar",
+    admin: true # make the first user an admin by default 
 )
 
 99.times do |n|
