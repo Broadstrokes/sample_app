539b1f5059508d4a62c83e6a2a534268930c9486
Dan Sajjad <dan.sajjad@gmail.com>
2018-11-13 06:43:02 +0000

    An integration test for delete links and destroying users; Finish user edit, update, index, and destroy actions

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,18 +2,37 @@ require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:michael)
+    @admin = users(:michael)
+    @non_admin = users(:archer)
   end
 
   test "index including pagination" do
-    log_in_as(@user)
+    log_in_as(@admin)
     get users_path
     assert_template 'users/index'
     # checking for a div with the required pagination class
     assert_select 'div.pagination'
     # and verifying that the first page of users is present
-    User.paginate(page: 1).each do |user|
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+      # checks for the right delete links, including skipping the 
+      # test if the user happens to be the admin
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
     end
+    
+    # checking that User.count changes by −1 when issuing a 
+    # delete request to the corresponding user path
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+  
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 end
