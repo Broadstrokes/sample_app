05e6cc4371306f137088bdf4040f1e11ab74391d
Dan Sajjad <dan.sajjad@gmail.com>
2018-11-13 06:35:58 +0000

    Action-level tests for admin access control

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -48,4 +48,23 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     # the new changes
     assert_not @other_user.reload.admin?
   end
+  
+  test "should redirect destroy when not logged in" do
+    # makes sure that the user count doesn’t change using the 
+    # assert_no_difference method
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    # makes sure that the user count doesn’t change using the 
+    # assert_no_difference method
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
 end
