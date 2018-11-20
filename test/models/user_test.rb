ddca00c8ccb2ab8ae99115660c1dbcd66709f863
Dan Sajjad <dan.sajjad@gmail.com>
2018-11-20 08:03:28 +0000

    Use the generalized authenticated? method in current_user & User test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -91,6 +91,6 @@ class UserTest < ActiveSupport::TestCase
   
   test 'authenticated? should return false for a user with nil digest' do
     # @user defined in the setup method does not have a remember_digest 
-    assert_not @user.authenticated?('') # it doesn't matter what the value of remember_token is
+    assert_not @user.authenticated?(:remember, '') # it doesn't matter what the value of remember_token is
   end
 end
