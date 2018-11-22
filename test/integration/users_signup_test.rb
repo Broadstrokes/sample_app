d428acd3012fc7d6c37974b067d68360cc4f3c58
Dan Sajjad <dan.sajjad@gmail.com>
2018-11-22 02:14:09 +0000

    Add account activation to the user signup test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
   
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+  
   test 'invald signup information' do
     get signup_path
     assert_select 'form[action="/signup"]'
@@ -23,12 +27,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
   
   
-  test 'successful signup' do
+  test 'successful signup with account activation' do
     get signup_path
     assert_select 'form[action="/signup"]'
     
     assert_difference 'User.count', 1 do
-      post signup_path, params: {
+      post users_path, params: {
         user: {
           name: 'Pink Panther',
           email: 'panther@gmail.com',
@@ -37,12 +41,30 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         }
       }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    # Try to log in before activation
+    log_in_as(user)
+    assert_not is_logged_in?
+    
+    # Invalid activation token
+    get edit_account_activation_path('invalid token', email: user.email)
+    assert_not is_logged_in?
+    
+    # Valid activation token, wrong email
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    
+    # Valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    
     # This simply arranges to follow the redirect after submission,
     # resulting in a rendering of the 'users/show' template
     follow_redirect!
-    # assert_template 'users/show'
-    # assert is_logged_in?
+    assert_template 'users/show'
+    assert is_logged_in?
     assert_not flash.empty?
-    # assert_select 'div.alert-success'
+    assert_select 'div.alert-success'
   end
 end
