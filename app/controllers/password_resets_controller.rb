3a95a8f0d22e0c09a2d1c6af75b7185d09d644d2
Dan Sajjad <dan.sajjad@gmail.com>
2018-12-02 04:38:59 +0000

    Create the reset edit action to get the edit form to render

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,9 @@
 class PasswordResetsController < ApplicationController
+  # Because the existence of a valid @user is needed in both the edit and update 
+  # actions, we’ll put the code to find and validate it in a couple of before filters
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  
   def new
   end
 
@@ -18,4 +23,17 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+
+  private
+    
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+    
+    # Confirms a valid user
+    def valid_user
+      unless(@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
 end
