873d6d35656e8f8ba78242820b6f6d06db009a19
Dan Sajjad <dan.sajjad@gmail.com>
2018-12-02 03:06:34 +0000

    Fix typo in host url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'https://sample-app-dansajjad.c9users.io/' # Don't use this literally; use your local dev host instead
+  host = 'https://sample-app-dansajjad.c9users.io' # Don't use this literally; use your local dev host instead
   # Use this on the cloud IDE.
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   # Use this if developing on localhost.
