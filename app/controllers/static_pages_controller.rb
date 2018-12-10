7f6277af8fae21b717f1cbfd902850c4d6d9b957
Dan Sajjad <dan.sajjad@gmail.com>
2018-12-10 03:37:12 +0000

    Add microposts creation to Home page, add partials, update display of error messages to work with other objects

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
+  
   def home
+    # Add a micropost instance variable to the home action
+    # current_user exists only if the user is logged in, so the 
+    # @micropost variable should only be defined in this case
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
