acbd18631fde2299ff7a38469171b433791278d6
Dan Sajjad <dan.sajjad@gmail.com>
2018-12-10 02:54:54 +0000

    Add Microposts controller create action

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,9 +1,24 @@
 class MicropostsController < ApplicationController
-    before_action :logged_in_user, only: [:create, :destroy]
-    
-    def create
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params) # use of strong parameters via micropost_params
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+   
+    # Permits only the micropost’s content attribute to be modified through the web
+    def micropost_params
+      params.require(:micropost).permit(:content)
     end
     
-    def destroy
-    end
 end
