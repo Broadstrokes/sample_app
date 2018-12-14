994f88a8210ff83c176cd62ab83d56d6c5784951
Dan Sajjad <dan.sajjad@gmail.com>
2018-12-14 08:44:09 +0000

    Add following/follower relationships to the seed data; db migrate reset seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,3 +45,11 @@ users = User.order(:created_at).take(6) # grab the 1st 6 users
     content = Faker::HarryPotter.quote.slice(0, 140)
     users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
