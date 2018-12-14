e3de78a09c3d67d0f30047e4f3309286720537e9
Dan Sajjad <dan.sajjad@gmail.com>
2018-12-14 08:25:19 +0000

    Followed: Add User model following xship; tests for 'following' helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
     has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    
                                   
     attr_accessor :remember_token, :activation_token, :reset_token
     
@@ -121,6 +123,21 @@ class User < ApplicationRecord
         # serious security hole called SQL injection. The id attribute here is 
         # just an integer (i.e., self.id, the unique ID of the user)
         Micropost.where("user_id = ?", id)
+    end
+    
+    # Follows a user
+    def follow(other_user)
+        following << other_user
+    end
+    
+    # Unfollows a user
+    def unfollow(other_user)
+        following.delete(other_user)
+    end
+    
+    # Returns true if the current user is following the other user
+    def following?(other_user)
+        following.include?(other_user)
     end
     
     private
